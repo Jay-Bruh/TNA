c124d9379646c847172421b7bc2e8ede3d71dfbe
Jay <50700356+Jay-Bruh@users.noreply.github.com>
2021-07-27 17:57:10 +0400

    Create Layout.js

---
+++ Frontend/TNA-master/nextjs/components/Layout.js
@@ -0,0 +1,9 @@
+import utilStyles from '../styles/utils.module.css'
+import styles from './layout.module.css'
+
+export default function Loader(){
+    return(
+        <div className={styles.loaderContainer}>
+        </div>
+    )
+}
