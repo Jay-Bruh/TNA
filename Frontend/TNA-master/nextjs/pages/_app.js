a2592fbd7ff8d7c53996651e89f8b6ac9fabf38d
Jay <50700356+Jay-Bruh@users.noreply.github.com>
2021-07-27 17:58:34 +0400

    Update _app.js

--- Frontend/TNA-master/nextjs/pages/_app.js
+++ Frontend/TNA-master/nextjs/pages/_app.js
@@ -1,5 +1,27 @@
-import '../styles/global.css'
+import '../styles/global.css';
+import {useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Loader from "../components/Loader"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => { //<-- this useEffect will be triggered just one time at component initialization
+      router.events.on("routeChangeStart", (url) => {
+         console.log("Route is changing");
+         setLoading(true)
+      });
+      router.events.on("routeChangeComplete", (url) => {
+         console.log("Route is changed");
+         setLoading(false)
+      });
+  }, []);
+
+  return (
+    <>
+    {loading && <Loader />}
+    <Component {...pageProps} />;
+    </>
+  );
 }
